b330b70d614e75aef652d467dd04698d8d7b5504
Timothy Okwii <tokwii@cisco.com>
2015-12-05 01:13:42 +0000

    Created Schedule Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204220756) do
+ActiveRecord::Schema.define(version: 20151205010024) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -30,5 +30,26 @@ ActiveRecord::Schema.define(version: 20151204220756) do
 
   add_index "models", ["email"], name: "index_models_on_email", unique: true
   add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "type"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "phone"
+    t.date     "avaliability_date"
+    t.time     "availability_time"
+    t.string   "referee_name"
+    t.string   "refree_email"
+    t.string   "referee_phone"
+    t.string   "referee_description"
+    t.string   "address"
+    t.integer  "rating"
+    t.string   "gender"
+    t.string   "decription"
+    t.integer  "verification"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
 end
